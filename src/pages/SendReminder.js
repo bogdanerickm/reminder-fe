0ad8915817e7b3734896c972f812184bb76ba5a0
Bogdan Matuszyczk <bogdan.utn@gmail.com>
2020-07-12 19:22:48 -0300

    add react hook form with validations

--- src/pages/SendReminder.js
+++ src/pages/SendReminder.js
@@ -5,13 +5,12 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import DateTimeField from '../components/DateTimeField';
-
+import { useForm } from "react-hook-form";
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -31,14 +30,31 @@ const useStyles = makeStyles((theme) => ({
   submit: {
     margin: theme.spacing(3, 0, 2),
   },
+  colorError: {
+    color: 'red'
+  }
 }));
 
 export default function SendReminder() {
-  const classes                         = useStyles();
-  const [email, setEmail]               = useState();
-  const [name, setName]                 = useState();
-  const [selectedDate, setSelectedDate] = useState();
-  const [selectedTime, setSelectedTime] = useState();
+  const classes                             = useStyles();
+  const [selectedDate, setSelectedDate]     = useState(new Date());
+  const [selectedTime, setSelectedTime]     = useState(new Date());
+  const [sendByTelegram, setSendByTelegram] = useState(false);
+  const [sendByEmail, setSendByEmail]       = useState(false);
+
+  const { register, handleSubmit, watch, errors } = useForm();
+
+  const onSubmit = data => {
+    console.log('datita',data);
+    const dataToSend = {
+      ...data,
+      selectedDate,
+      selectedTime,
+      sendByTelegram,
+      sendByEmail
+    }
+    console.log('datita completa',dataToSend);
+  }
   
   const handleDateChange = (date) => {
     setSelectedDate(date);
@@ -47,7 +63,7 @@ export default function SendReminder() {
   const handleTimeChange = (time) => {
     setSelectedTime(time);
   };
-  
+
   return (
     <Container component="main" maxWidth="xs">
       <CssBaseline />
@@ -57,33 +73,40 @@ export default function SendReminder() {
         <Typography component="h1" variant="h5">
           Reminder
         </Typography>
-        <form className={classes.form} noValidate>
+        <form className={classes.form} onSubmit={handleSubmit(onSubmit)}>
           <Grid container spacing={2}>
               <DateTimeField date={selectedDate} dateHandler={handleDateChange} time={selectedTime} timeHandler={handleTimeChange}/>
             <Grid item xs={12} sm={6}>
-              <TextField autoComplete="fname" name="firstName" variant="outlined" required fullWidth id="firstName" label="First Name" autoFocus />
+              <TextField variant="outlined" inputRef={register({ required: true, minLength:1, maxLength: 10 })} name="firstName" fullWidth label="First Name" />
+              <p className={classes.colorError}>{errors.firstName && errors.firstName.message}</p>
             </Grid>
             <Grid item xs={12} sm={6}>
-              <TextField variant="outlined" required fullWidth id="lastName" label="Last Name" name="lastName" autoComplete="lname" />
+              <TextField variant="outlined" inputRef={register({ required: true, minLength:1, maxLength: 10 })} fullWidth label="Last Name" name="lastName" />
+              {errors.lastName && errors.lastName.message}
             </Grid>
             <Grid item xs={12}>
-              <TextField variant="outlined" required fullWidth id="email" label="Email Address" name="email" autoComplete="email" />
+              <TextField variant="outlined" inputRef={register({ required: true, pattern: { value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i, message: "invalid email address"}})} fullWidth label="Email from" name="emailFrom" />
+              <p className={classes.colorError}>{errors.emailFrom && errors.emailFrom.message}</p>
             </Grid>
             <Grid item xs={12}>
-              <TextField variant="outlined" required fullWidth name="password" label="Password" type="password" id="password" autoComplete="current-password" />
+              <TextField variant="outlined" inputRef={register({ required: false, pattern: { value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i, message: "invalid email address"}})} fullWidth label="Email to" name="emailTo"/>
+              <p className={classes.emailTo}>{errors.emailTo && errors.emailTo.message}</p>
+            </Grid>
+            <Grid item xs={12}>
+              <TextField variant="outlined" inputRef={register({ required: true, maxLength: 40 })} fullWidth multiline rows={6} name="body" label="body" type="text" />
             </Grid>
             <Grid item xs={12}>
               <FormControlLabel
-                control={<Checkbox value="allowExtraEmails" color="primary" />}
+                control={<Checkbox value={sendByTelegram} onChange={()=> setSendByTelegram(!sendByTelegram)} color="primary" />}
                 label="I want to receive Telegram Notification."
               />
               <FormControlLabel
-                control={<Checkbox value="allowExtraEmails" color="primary" />}
+                control={<Checkbox value={sendByEmail} onChange={()=> setSendByEmail(!sendByEmail)} color="primary" />}
                 label="I want to receive an Email Notification."
               />
             </Grid>
           </Grid>
-          <Button type="submit" fullWidth variant="outlined" color="primary" className={classes.submit} >
+          <Button fullWidth type="submit" variant="outlined" color="primary" className={classes.submit} >
             Send
           </Button>
         </form>
